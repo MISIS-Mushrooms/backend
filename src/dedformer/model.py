976fe4c0f9fce3d7a7e57310c76fbf0f7c29df87
mrapplexz <mr.applexz@gmail.com>
2023-05-26 03:07:11 +0300

    attention pooling

--- src/dedformer/model.py
+++ src/dedformer/model.py
@@ -1,8 +1,10 @@
 import random
 import re
 import warnings
+from collections import defaultdict
 from typing import Tuple, Set, Dict, List, Optional, FrozenSet
 
+import numpy as np
 import pandas as pd
 import torch
 from pytorch_metric_learning.distances import CosineSimilarity
@@ -17,7 +19,7 @@ from torchmetrics.functional import pairwise_cosine_similarity
 from tqdm import tqdm
 from transformers import RobertaConfig, T5Config
 from transformers.modeling_utils import ModuleUtilsMixin
-from transformers.models.roberta.modeling_roberta import RobertaEncoder, RobertaPooler
+from transformers.models.roberta.modeling_roberta import RobertaEncoder, RobertaPooler, RobertaSelfAttention
 from xztrainer import XZTrainable, ContextType, BaseContext, DataType, ModelOutputsType
 
 from dedformer.arcface import ArcFace
@@ -172,22 +174,45 @@ class AttendanceDataset(Dataset):
         user_id = self._attend_indices[index]
         user_data = self._bank_user.to_model_data(user_id)
         item = self._attend[user_id]
-        group_nums = item['group_sequence']
+
+        visited = set()
+        group_nums = []
+        target_first_seen = None
+        target = None
+        for row in item.itertuples():
+            if row.macro_id not in visited:
+                visited.add(row.macro_id)
+                group_nums.append(row.macro_id)
+                target_first_seen = row.dt
+                target = row.macro_id
+        group_nums = group_nums[:-1]
+
+        group_data_by_macro = item.groupby('macro_id')
+        group_times, group_min_dt, group_max_dt, group_often_time = [], [], [], []
+        for mac in group_nums:
+            mac_data = group_data_by_macro.get_group(mac)
+            mac_data = mac_data[mac_data['dt'] <= target_first_seen]
+            group_times.append(int(mac_data['dt'].count()))
+            group_min_dt.append(mac_data['dt'].min().to_pydatetime())
+            group_max_dt.append(mac_data['dt'].max().to_pydatetime())
+            # group_often_time.append(mac_data['dt'].dt.time.mode().iloc[0])
+        group_times_sum = sum(group_times)
+        if group_times_sum == 0 and len(group_times) > 0:
+            print(123)
+
+
         # if self._is_train:
         #     min_length = 1
         #     group_start = random.randint(0, len(group_nums) - min_length)
         #     group_end = group_start + random.randint(group_start, len(group_nums) - 1)
         #     group_nums = group_nums[group_start:group_end + 1]
-        group_nums = group_nums[::-1][:500][::-1]
-        if self._dummy:
-            i = random.randint(0, self._bank_group.macro_group_num() - 1)
-            group_nums = [i for _ in range(random.randint(3, 40))]
         return {
-            'group_sequence': torch.LongTensor(group_nums[:-1]),
-            'group_sequence_out': torch.LongTensor(group_nums),
-            'group_target': torch.scalar_tensor(group_nums[-1], dtype=torch.long),
-            'group_pos': torch.LongTensor(list(range(len(group_nums)))[::-1]),
-            'group_mask': torch.LongTensor([1 for _ in range(len(group_nums))]),
+            'group_sequence': torch.LongTensor(group_nums),
+            'group_popularity': torch.FloatTensor([x / group_times_sum for x in group_times]),
+            'group_sequence_out': torch.LongTensor(group_nums + [target]),
+            'group_target': torch.scalar_tensor(target, dtype=torch.long),
+            'group_pos': torch.LongTensor(list(range(len(group_nums) + 1))[::-1]),
+            'group_mask': torch.LongTensor([1 for _ in range(len(group_nums) + 1)]),
             **user_data
         }
 
@@ -207,7 +232,8 @@ class AttendanceCollator():
             'group_mask': torch.stack([F.pad(x['group_mask'], (0, max_group_mask_ln - len(x['group_mask']))) for x in batch]),
             'group_sequence_out': torch.stack([F.pad(x['group_sequence_out'], (0, max_group_sequence_ln_out - len(x['group_sequence_out']))) for x in batch]),
             'user_gender': torch.stack([x['user_gender'] for x in batch]),
-            'user_age_bin': torch.stack([x['user_age_bin'] for x in batch])
+            'user_age_bin': torch.stack([x['user_age_bin'] for x in batch]),
+            'group_popularity': torch.stack([F.pad(x['group_popularity'], (0, max_group_sequence_ln - len(x['group_popularity']))) for x in batch])
         }
 
 
@@ -243,8 +269,7 @@ class GroupVectorizer(nn.Module):
         self._act_3_emb = nn.Embedding(len(all_actions[2]), EMBED_DIM)
         self._area_emb = nn.Embedding(len(all_areas), EMBED_DIM)
         self._block_emb = nn.Embedding(len(all_blocks), EMBED_DIM)
-        self._W = nn.Parameter(torch.empty([5, 1, 1]))
-        nn.init.uniform_(self._W, 0.3, 0.7)
+        self._aten = RobertaSelfAttention(RobertaConfig(hidden_size=EMBED_DIM, num_attention_heads=2))
 
     def forward(self, data):
         act_1_emb = self._act_1_emb(data['action_1'])
@@ -254,9 +279,8 @@ class GroupVectorizer(nn.Module):
         block_emb = self._block_emb(data['block'])
         area_emb = (data['area_mask'].unsqueeze(2) * area_emb).sum(dim=1) / data['area_mask'].sum(dim=1).clamp(min=1).unsqueeze(1)
         block_emb = (data['block_mask'].unsqueeze(2) * block_emb).sum(dim=1) / data['block_mask'].sum(dim=1).clamp(min=1).unsqueeze(1)
-        all_emb = torch.stack([act_1_emb, act_2_emb, act_3_emb, area_emb, block_emb])
-        all_emb = self._W * all_emb
-        all_emb = all_emb.sum(dim=0)
+        all_emb = torch.stack([act_1_emb, act_2_emb, act_3_emb, area_emb, block_emb], dim=1)
+        all_emb = self._aten(all_emb)[0].sum(dim=1)
         return all_emb
 
 
@@ -279,6 +303,7 @@ class AllVectorizer(nn.Module):
         self._user_vec = UserVectorizer(bank_users)
         self._all_group_index = nn.Parameter(torch.LongTensor(list(range(bank.num_macro_groups))), requires_grad=False)
         self._all_group_data = nn.ParameterDict({k: nn.Parameter(v, requires_grad=False) for k, v in bank.get_model_data(list(range(bank.num_macro_groups))).items()})
+        self._popularity_enc = nn.Sequential(nn.Linear(1, EMBED_DIM), nn.GELU(), nn.Linear(EMBED_DIM, EMBED_DIM))
         self._sos = nn.Parameter(torch.empty([1, 1, EMBED_DIM]))
         nn.init.normal_(self._sos)
         self._pos_enc = nn.Embedding(512, EMBED_DIM)
@@ -294,6 +319,7 @@ class AllVectorizer(nn.Module):
         )
         self._t5 = T5ForConditionalGeneration(cfg)
         self._loss = ArcFace()
+        self._decoder_feature_aten = RobertaSelfAttention(RobertaConfig(hidden_size=EMBED_DIM, num_attention_heads=1))
         # self._loss = TripletMarginLoss(distance=CosineSimilarity())
 
     def forward(self, data):
@@ -306,6 +332,12 @@ class AllVectorizer(nn.Module):
         x_dec = all_group_vec[group_seq]
         # pos_ids = torch.arange(curr_vec.shape[1] - 1, -1, -1)
         x_dec = x_dec + self._pos_enc(group_pos)
+        # x_dec = self._decoder_feature_aten(
+        #     torch.stack([
+        #         x_dec,
+        #         self._pos_enc(group_pos),
+        #         self._popularity_enc(data['group_popularity'].unsqueeze(2))
+        #     ], dim=-2).view(-1, 3, EMBED_DIM))[0].sum(dim=1).view(group_seq.shape[0], group_seq.shape[1], EMBED_DIM)
         x_dec = torch.cat([self._sos.repeat(x_dec.shape[0], 1, 1), x_dec], dim=1)
         enc_x, enc_mask = self._user_vec(data)
         # group_mask = get_extended_attention_mask(group_mask, dtype=x.dtype)
--- src/dedformer/process_attendance.py
+++ src/dedformer/process_attendance.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import pickle
+from collections import defaultdict
 
 import pandas as pd
 from pandarallel import pandarallel
@@ -22,20 +23,10 @@ if __name__ == '__main__':
     att['dt'] = att.parallel_apply(lambda x: dt.datetime.fromisoformat(f'{x["дата занятия"]}T{x["время начала занятия"]}'), axis=1)
     for group_name, group_data in tqdm(att.groupby('уникальный номер участника')):
         group_data = group_data.sort_values(by='dt')
-        group_seq = list(group_data['уникальный номер группы'])
-        group_seq_macro = list([bank.get_macro_group_id(x) for x in group_seq])
-        visited = set()
-        group_seq_macro_unique = []
-        for i in group_seq_macro:
-            if i not in visited:
-                visited.add(i)
-                group_seq_macro_unique.append(i)
-        if len(group_seq_macro_unique) == 0:
-            continue
-        dct = {
-            'group_sequence': group_seq_macro_unique
-        }
-        all_data[group_name] = dct
+        group_data.drop(columns=['уникальный номер занятия', 'уникальный номер участника', 'направление 2', 'направление 3', 'онлайн/офлайн', 'дата занятия', 'время окончания занятия'], inplace=True)
+        group_data['macro_id'] = group_data['уникальный номер группы'].apply(lambda x: bank.get_macro_group_id(x))
+
+        all_data[group_name] = group_data
     with open('data/attendance.pkl', 'wb') as f:
         pickle.dump(all_data, f)
     print(123)
